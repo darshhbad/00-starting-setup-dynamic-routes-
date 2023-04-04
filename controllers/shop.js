ddc597a86014cf26d85f22797a0c0cbe69b99cbc
darshhbad <darsh.badukle.che17@itbhu.ac.in>
2023-04-04 16:38:56 +0530

    adding  cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart=require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -17,6 +18,8 @@ exports.getProduct=(req,res,next)=>{
   })
 }
 
+
+
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/index', {
@@ -33,6 +36,14 @@ exports.getCart = (req, res, next) => {
     pageTitle: 'Your Cart'
   });
 };
+
+exports.postCart = (req,res,next)=>{
+  const prodId=req.body.productId
+ Product.findById(prodId, (product)=>{
+  Cart.addProduct(prodId,product.price)
+ })
+  res.redirect('/cart')
+}
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
